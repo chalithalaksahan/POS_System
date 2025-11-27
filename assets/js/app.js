0b35c1cab9dae96f7442bfcc3fe3855f078920e3
Chalitha Laksahan <141056409+chalithalaksahan@users.noreply.github.com>
2025-11-27 23:42:26 +0530

    add toggle buttons

---
+++ assets/js/app.js
@@ -0,0 +1,46 @@
+console.log("Js loaded");
+
+let mainContainer = document.getElementById("mainContainer");
+const pos = document.getElementById("pos");
+const product = document.getElementById("product");
+const customer = document.getElementById("customer");
+const order = document.getElementById("order");
+
+
+function clearBtn(){
+document.querySelectorAll('.category-btn').forEach(btn => {
+    btn.classList.remove('bg-[#36BBA7]','text-white');
+    btn.classList.add('bg-white','text-black');
+});
+
+}
+
+function posInterface(){
+    clearBtn();
+    pos.classList.remove('bg-white','text-black');
+    pos.classList.add('bg-[#36BBA7]','text-white');
+}
+
+function productInterface(){ 
+    clearBtn();
+    product.classList.remove('bg-white','text-black');
+    product.classList.add('bg-[#36BBA7]','text-white'); 
+}
+
+function customerInterface(){ 
+    clearBtn();
+    customer.classList.remove('bg-white','text-black');
+    customer.classList.add('bg-[#36BBA7]','text-white'); 
+}
+
+function orderInterface(){
+    clearBtn();
+    order.classList.remove('bg-white','text-black');
+    order.classList.add('bg-[#36BBA7]','text-white');
+}
+
+pos.addEventListener('click',posInterface);
+product.addEventListener('click',productInterface);
+customer.addEventListener('click',customerInterface);
+order.addEventListener('click',orderInterface);
+
